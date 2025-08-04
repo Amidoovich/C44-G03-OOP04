17296ff57a7a9f78a8e38eb3702ff6ea4633e180
Amidooovich <144342730+Amidoovich@users.noreply.github.com>
2025-08-05 01:25:52 +0300

    Part 03

--- S4/Program.cs
+++ S4/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace S4
+﻿using System;
+using S4.Overloading;
+
+namespace S4
 {
     internal class Program
     {
@@ -28,14 +31,42 @@
         }
         static void Main(string[] args)
         {
-            //double A = 10.2, B = 20.3 , C = 30;
+            #region Methods OverLoading
+            //double A = 10.2, B = 20.3, C = 30;
             //int Result = SumTwoNumbers(A, B);
-            //int Result = SumThreeNumbers(A,B,C);
-            //double Result = SumNumbers(A,B);
-            //int Result = SumNumbers(10,20,30);
+            //int Result = SumThreeNumbers(A, B, C);
+            //double Result = SumNumbers(A, B);
+            //int Result = SumNumbers(10, 20, 30);
             //Console.WriteLine(Result);
 
-            //Console.WriteLine();
+            //Console.WriteLine(); 
+            #endregion
+
+            #region MyRegion
+
+            // 5 + 4i
+            Complex C1 = new Complex()
+            {
+                Real = 10,
+                Img = 5
+            };
+            Console.WriteLine($"C1 = {C1}");
+
+            Complex C2 = new Complex()
+            {
+                Real = 6,
+                Img = 2
+            };
+            Console.WriteLine($"C2 = {C2}");
+
+            Complex C3 = C1 - C2;
+            Console.WriteLine("======================");
+            Console.WriteLine($"C3 = {C3}");
+            
+
+
+            #endregion
+
         }
     }
 }
