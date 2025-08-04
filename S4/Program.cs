f4cb9c4160f9f242506173bd4c8d389f16fae99b
Amidooovich <144342730+Amidoovich@users.noreply.github.com>
2025-08-05 00:58:02 +0300

    Part 02

--- S4/Program.cs
+++ S4/Program.cs
@@ -2,9 +2,40 @@
 {
     internal class Program
     {
+        static int SumNumbers(int x, int y)
+        {
+            return x + y;
+        }
+        //static double SumNumbers(int x, int y)
+        //{
+        //    return x + y;
+        //}
+        static int SumNumbers(int x,int y,int z)
+        {
+            return x + y + z;
+        }
+        static double SumNumbers(double a,double b)
+        {
+            return a + b;
+        }
+        static double SumNumbers(double a,int b)
+        {
+            return a + b;
+        }
+        static double SumNumbers(int a,double b)
+        {
+            return a + b;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //double A = 10.2, B = 20.3 , C = 30;
+            //int Result = SumTwoNumbers(A, B);
+            //int Result = SumThreeNumbers(A,B,C);
+            //double Result = SumNumbers(A,B);
+            //int Result = SumNumbers(10,20,30);
+            //Console.WriteLine(Result);
+
+            //Console.WriteLine();
         }
     }
 }
