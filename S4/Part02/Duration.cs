a644e0d67383bb5d2c6cd4a74f396df55be06630
Amidooovich <144342730+Amidoovich@users.noreply.github.com>
2025-08-06 21:04:53 +0300

    Part02 Q4

--- S4/Part02/Duration.cs
+++ S4/Part02/Duration.cs
@@ -62,6 +62,84 @@ namespace S4.Part02
 
         }
 
+        public int ToSeconds()
+        {
+            return (Hours * 3600) + (Minutes * 60)  + Seconds;
+        }
+
+        #endregion
+
+        #region Operator overLoading
+
+        public static Duration operator +(Duration Left , Duration Right)
+        {
+            return new Duration((Left?.ToSeconds() ?? 0 ) + (Right?.ToSeconds() ?? 0));
+
+        }
+        public static Duration operator +(Duration Left , int Right)
+        {
+
+            return new Duration((Left?.ToSeconds() ?? 0) + Right);
+ 
+        }
+        public static Duration operator +(int Left , Duration Right)
+        {
+
+            return new Duration(Left + (Right?.ToSeconds() ?? 0));
+     
+        }
+        public static Duration operator ++(Duration D)
+        {
+            return new Duration((D?.ToSeconds() ?? 0) + 60);
+
+        }
+        public static Duration operator --(Duration D)
+        {
+            return new Duration((D?.ToSeconds() ?? 0) - 60);
+
+        }
+        public static Duration operator -(Duration Left, Duration Right)
+        {
+            return new Duration((Left?.ToSeconds() ?? 0) - (Right?.ToSeconds() ?? 0));
+
+        }
+
+        public static bool operator >(Duration Left, Duration Right)
+        {
+            return Left.ToSeconds() > Right.ToSeconds();
+        }
+        public static bool operator <(Duration Left, Duration Right)
+        {
+            return Left.ToSeconds() < Right.ToSeconds();
+        }
+        public static bool operator <=(Duration Left, Duration Right)
+        {
+            return Left.ToSeconds() <= Right.ToSeconds();
+        }
+        public static bool operator >=(Duration Left, Duration Right)
+        {
+            return Left.ToSeconds() >= Right.ToSeconds();
+        }
+
+        public static bool operator true(Duration D)
+        {
+            return D.ToSeconds() > 0;
+        }
+        public static bool operator false(Duration D)
+        {
+            return D.ToSeconds() <= 0;
+        }
+
+
+        #endregion
+
+        #region Casting Operator
+
+        public static explicit operator DateTime(Duration D)
+        {
+            return new DateTime(DateTime.Now.Year,DateTime.Now.Month,DateTime.Now.Day,D.Hours,D.Minutes,D.Seconds);
+        }
+
         #endregion
     }
 }
